5bd8fee47e88c292276321e6ebc1ec1ae431b3f0
Axel Meier <axel.meier@akquinet.de>
2016-11-02 11:42:41 +0100

    Removed superfluous MessageBundles class

--- notebook-ui/src/main/java/de/akquinet/engineering/notebook/ui/i18n/I18nImpl.java
+++ notebook-ui/src/main/java/de/akquinet/engineering/notebook/ui/i18n/I18nImpl.java
@@ -3,8 +3,9 @@ package de.akquinet.engineering.notebook.ui.i18n;
 import com.vaadin.cdi.UIScoped;
 import com.vaadin.ui.UI;
 
-import javax.inject.Inject;
+import java.text.MessageFormat;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * @author Axel Meier, akquinet engineering GmbH
@@ -12,11 +13,24 @@ import java.util.Locale;
 @UIScoped
 public class I18nImpl implements I18n
 {
-    @Inject
-    private MessageBundles bundles;
+    private static final String BASE_NAME = "messages";
 
     public I18nImpl()
     {
+    }
+
+    private ResourceBundle getBundle(final Locale locale){
+        final ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, locale);
+        return bundle != null ? bundle : ResourceBundle.getBundle(BASE_NAME, Locale.US);
+    }
+
+    private String getByLocale(final Locale locale, final String key){
+        return getBundle(locale).getString(key);
+    }
+
+    private String getByLocale(final Locale locale, final String key, final Object... params){
+        final MessageFormat messageFormat = new MessageFormat(getByLocale(locale, key), locale);
+        return messageFormat.format(params);
     }
 
     private Locale getCurrentLocale(){
@@ -26,12 +40,12 @@ public class I18nImpl implements I18n
     @Override
     public String get(final String key)
     {
-        return bundles.get(getCurrentLocale(), key);
+        return getByLocale(getCurrentLocale(), key);
     }
 
     @Override
     public String get(final String key, final Object... params)
     {
-        return bundles.get(getCurrentLocale(), key, params);
+        return getByLocale(getCurrentLocale(), key, params);
     }
 }
